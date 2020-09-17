0867627b1c0052f807dbb6455c5c96affb4fc0c5
kupaeva <d.kupaeva@gmail.com>
2020-09-17 23:33:35 +0300

    merge with new_branch

--- converter.py
+++ converter.py
@@ -1,37 +1,33 @@
 import sys
 units_dict = ['C', 'F', 'Ra', 'K']
 
-
-# convert any units to Celsius
-def to_celsius():
+# convert any units to celsia
+def to_celsia(units_from, value):
     if units_from == 'F':
-        celsius_value_local = (value - 32) / 1.8
+        celsia_value = (value - 32) / 1.8
     elif units_from == 'K':
-        celsius_value_local = (value - 273.15)
+        celsia_value = (value - 273.15)
     elif units_from == 'Ra':
-        celsius_value_local = (value - 491.67) * 5 / 9
-    return celsius_value_local
+        celsia_value = (value - 491.67) * 5 / 9
+    return celsia_value
 
 
-# convert celsius to any units
-def from_celsius():
+# convert celsia to any units
+def from_celsia(units_to, celsia_value):
     if units_to == 'F':
-        units_value_local = (celsius_value * 1.8) + 32
+        units_value = (celsia_value * 1.8) + 32
     elif units_to == 'K':
-        units_value_local = (celsius_value + 273.15)
+        units_value = (celsia_value + 273.15)
     elif units_to == 'Ra':
-        units_value_local = (celsius_value + 273.15) * 9 / 5
-    return units_value_local
+        units_value = (celsia_value + 273.15) * 9 / 5
+    return units_value
 
-
-# input data
+#input data
 print("Hello! I am temperature unit converter. "
-      "I know next scales: Celsius (C), Fahrenheit (F), Rankine (Ra) "
-      "and absolute Kelvin temperature (K).")
+      "I know next scales: Celsius (C), Fahrenheit (F), Rankine (Ra) and absolute Kelvin temperature (K).")
 units_from = input('Please, input original units (C, F, Ra or K)\n').title()
 units_to = input('Please, input output units (C, F, Ra or K)\n').title()
 value = input('Please, input value\n')
-
 
 # check input to compliance with acceptable
 if units_from not in units_dict or units_to not in units_dict:
@@ -42,17 +38,16 @@ elif units_from == units_to:
     print(value, units_from, 'is', value, units_to)
     sys.exit()
 
-
-# convert any units to celsius and celsius to any units, print output
+# convert any units to celsia and celsia to any units, print output
 else:
     value = float(value)
     if units_from == 'C':
-        celsius_value = value
+        celsia_value = value
     elif units_from != 'C':
-        celsius_value = to_celsius()
+        celsia_value = to_celsia(units_from, value)
 
     if units_to == 'C':
-        print(value, units_from, 'is', celsius_value, units_to)
+        print(value, units_from, 'is', celsia_value, units_to)
     elif units_to != 'C':
-        celsius_value = from_celsius()
-        print(value, units_from, 'is', celsius_value, units_to)
+        celsia_value = from_celsia(units_to, celsia_value)
+        print(value, units_from, 'is', celsia_value, units_to)
