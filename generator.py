f28a00a133d259e23566513fb7a48fa377653e73
kupaeva <51483087+kupaeva@users.noreply.github.com>
2021-02-27 13:56:09 +0300

    generators

---
+++ generator.py
@@ -0,0 +1,26 @@
+import itertools
+from Bio import SeqIO
+
+
+def generate(n):
+    elements = 'ATGC'
+    for len in range(1, n + 1):
+        for i in itertools.combinations_with_replacement(elements, len):
+            yield (''.join(i))
+
+
+def translator(path):
+    file = SeqIO.parse(path, "fasta")
+    for seq_record in file:
+        yield (seq_record.seq.translate())
+
+
+length = int(input('Input maximal length of sequense'))
+a = generate(length)
+
+for i in a:
+    print(i)
+
+b = translator('D:/YandexDisk/хакатон/fishes/Danio.fasta')
+for i in b:
+    print(i)
